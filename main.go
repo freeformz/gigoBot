3917497c5dcfc0854d85792501b0c60cd73c3aed
Александр Попов <apopov@gambramedical.ru>
2015-11-07 01:51:50 +0300

    - Init

--- main.go
+++ main.go
@@ -5,21 +5,57 @@ import (
 	"github.com/subosito/gotenv"
 	"os"
 	"log"
+	"strings"
+	"strconv"
+	"math/rand"
+	"time"
 )
 
 func init() {
 	gotenv.Load()
 }
+var results map[string]int;
 
-func messageHandler(channel chan string, message string) {
-	channel<-"Echo: "+message
+func messageHandler(chat gitterClient.ChatStruct, message gitterClient.MessageStruct) {
+	if (strings.Contains(message.Text, "крутите барабан") || strings.Contains(message.Text, "крутить барабан")) {
+		r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+		roulette := [...]int{0,10,100,30,50,25,-1,0,50,0,10,25,5,150,-1,15,100,30,0,25,5,50,0,1000,150,25,10,30,-1,5,250,25,0,10,50,30,100,-1,25,50 }
+
+		localSpore := roulette[r.Intn(len(roulette)-1)]
+		if (localSpore == -1) {
+			chat.SendMessage("@"+message.FromUser.Username+" Вы банкрот!")
+			delete(results, message.FromUser.ID)
+		} else {
+			chat.SendMessage("@"+message.FromUser.Username+"  У вас "+strconv.Itoa(localSpore)+" очков")
+			if oldValue, ok := results[message.FromUser.ID]; ok {
+				results[message.FromUser.ID] = localSpore+oldValue
+			} else {
+				results[message.FromUser.ID] = localSpore
+			}
+		}
+	}
+
+	if (strings.Contains(message.Text, "приз")) {
+		count := 0
+		if oldValue, ok := results[message.FromUser.ID]; ok {
+			count = oldValue;
+		}
+		chat.SendMessage("@"+message.FromUser.Username+" Всего вы заработали: "+strconv.Itoa(count)+" очков")
+	}
 }
 
 func main() {
+	results = make(map[string]int)
 	token := os.Getenv("GITTER_API_TOKEN")
 
+
+	gitter := gitterClient.Create(token)
+
+
 	log.Print("Join channel debugChannel")
-	debugChannel := gitterClient.Chat{ TokenApi: token, RoomId:"563b92da16b6c7089cb99c97", Channel: make(chan string)  }
+	debugChannel := gitterClient.ChatStruct{ TokenApi: token, RoomId:"563b92da16b6c7089cb99c97", Channel: make(chan gitterClient.MessageStruct)  }
+	debugChannel.InfoMessage("Новая игра! Вы можете \"крутить барабан\" или получить \"приз\"", 60)
 	go debugChannel.JoinRoom()
 
 	/*
@@ -35,7 +71,7 @@ func main() {
 	for {
 		select {
 			case message:=<-debugChannel.Channel:
-				messageHandler(debugChannel.Channel, message)
+				messageHandler(debugChannel, message)
 			/*
 			case message:=<-secondChannel.Channel:
 				messageHandler(secondChannel.Channel, message)
